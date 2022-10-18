bc7add6c7705be98711f6a92a1bf2b9d1e87f24e
richard <wiryadinatarichard1@gmail.com>
2022-10-18 14:02:49 +0700

    first commit

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    public function index (){
+        $data = Employee::all();
+        // dd($data);
+        return view('dataPegawai', compact('data'));
+    }
+}
---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('employees')->insert([
+            'nama' => 'Richard',
+            'jenisKelamin' => 'laki-laki',
+            'noTelepon' => '0728173871',
+        ]);
+    }
+}
